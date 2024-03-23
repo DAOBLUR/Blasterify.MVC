ab5844b2f0707b231bf23456bb201a0df03af29b
DAOBLUR <102988549+DAOBLUR@users.noreply.github.com>
2024-03-23 05:23:12 -0500

    second commit

---
+++ Blasterify.Client/Controllers/AccessController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blasterify.Client.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SignUp(object client)
+        {
+            //return View();
+
+            return RedirectToAction("Index", "Access");
+        }
+
+        [HttpPost]
+        public ActionResult Index(string email, string password)
+        {
+            return View();
+        }
+    }
+}
---
+++ Blasterify.Client/Controllers/Shop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blasterify.Client.Controllers
+{
+    public class Shop : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
